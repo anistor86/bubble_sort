3fd0763ba58f638b3d12523849fff0b796c356a3
Luca Storani <lucastorani@gmail.com>
2017-07-22 16:05:18 +0100

    Fix method to accept a block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,27 +1,35 @@
 #bubble_sort method, takes an array and sort it with bubble sort
 def bubble_sort(array)
-  #loop until everything is swapped (sorted)
-  loop do
-    swapped = false
+  raise unless array.is_a?(Array)
 
-    (array.length - 1).times do |i|
-      #check if the array[i+1] element is bigger than array[i] element
-      if (array[i+1].to_i < array[i].to_i)
-        #swap position if bigger & set swapped to true
-        array[i], array[i+1] = array[i+1], array[i]
-        swapped = true
+  if array.empty? || array.length == 1
+    return array
+  else
+    #loop until everything is swapped (sorted)
+    loop do
+      swapped = false
+
+      (array.length - 1).times do |i|
+        #check if a block is given
+        if block_given?
+          if yield(array[i], array[i+1]) > 0
+            array[i], array[i+1] = array[i+1], array[i]
+            swapped = true
+          end
+        else
+          if (array[i+1].to_i < array[i].to_i)
+            #swap position if bigger & set swapped to true
+            array[i], array[i+1] = array[i+1], array[i]
+            swapped = true
+          end
+        end
       end
+
+      #if nothing is swapped (the array is sorted) will BREAK the loop
+      break if !swapped
     end
-
-    #if nothing is swapped (so the array is sorted), the method will never
-    #enter the IF and the swapped variable would be false and will BREAK
-    #if swapped is true it just jump the break and go back to the loop
-    break if !swapped
+    return array
   end
-
-  #print inspect and return the array
-  puts array.inspect
-  return array
 end
 
-bubble_sort([4,3,78,2,0,2])
+puts bubble_sort([4,3,78,2,0,2])
